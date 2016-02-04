80a2f7b06968a1382b1229bf313697e75529947a
Sanath <sanath.mullapudi@gmail.com>
2016-02-04 06:23:08 -0800

    made user links

--- routes/index.js
+++ routes/index.js
@@ -9,17 +9,12 @@ var User = require('../models/user');
 router.get('/', authMW, function(req, res, next) {
 	if (req.user) {
 		User.findById(req.user._id, function(err, user){
-      if (err) return res.status(400).send(err);
-      res.render('userPage', {user: user, admin: true});
-    })
+	      if (err) return res.status(400).send(err);
+	      res.render('userPage', {user: user, admin: true});
+	    })
 	}
 	else
 		res.render('index');
 });
-
-// router.get('/secret', authMW, function(req, res, next) {
-//   console.log('req.user:', req.user);
-//   res.send('authorized user');
-// });
 
 module.exports = router;
