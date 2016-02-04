e52802c064be47df0dea9e982502794fda55d2c3
Sanath <sanath.mullapudi@gmail.com>
2016-02-04 07:19:06 -0800

    made inventory jade and ui/ux stuff

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var authMW = require('../config/auth');
 
 var User = require('../models/user');
+var Item = require('../models/item');
+
 
 
 router.get('/', authMW, function(req, res, next) {
@@ -11,6 +13,17 @@ router.get('/', authMW, function(req, res, next) {
 		User.findById(req.user._id, function(err, user){
 	      if (err) return res.status(400).send(err);
 	      res.render('userPage', {user: user, admin: true});
+	    })
+	}
+	else
+		res.render('index');
+});
+
+router.get('/myGoods', authMW, function(req, res, next) {
+	if (req.user) {
+		Item.find({user:req.user._id}, function(err, items){
+	      if (err) return res.status(400).send(err);
+	      res.render('myGoods');
 	    })
 	}
 	else
