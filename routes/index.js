fa9b6813d9c9fbd32b94cf2a2d5cd3e6bf559797
Sanath <sanath.mullapudi@gmail.com>
2016-02-03 18:20:52 -0800

    added jade files, splash page working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var authMiddleware = require('../config/auth');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { title: "Cow Meets Goat" });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/secret', authMiddleware, function(req, res, next) {
+  console.log('req.user:', req.user);
+  res.send('authorized user');
 });
 
 module.exports = router;
